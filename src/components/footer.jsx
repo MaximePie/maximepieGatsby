a64d3f030166ee7a1ce985cb1bf859555e64a260
Chatmalow <maxime.pie.mail@gmail.com>
2021-04-14 11:13:31 +0200

    feat: Add responsive design

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,11 +8,13 @@ export default function Component() {
       <a className="Footer__link Footer__link--text" href="https://www.linkedin.com/in/maxime-pie-449149123/">
         Créé par Maxime Pie
       </a>
-      <a className="Footer__link" href="https://www.linkedin.com/in/maxime-pie-449149123/">
-        <i className="fab fa-linkedin"/>
-      </a>
-      <a className="Footer__link" href="https://github.com/MaximePie"><i className="fab fa-github"/></a>
-      <a className="Footer__link" href="https://www.youtube.com/user/Chamalowat"><i className="fab fa-youtube"/></a>
+      <span className="Footer__social-medias">
+        <a className="Footer__link" href="https://www.linkedin.com/in/maxime-pie-449149123/">
+          <i className="fab fa-linkedin"/>
+        </a>
+        <a className="Footer__link" href="https://github.com/MaximePie"><i className="fab fa-github"/></a>
+        <a className="Footer__link" href="https://www.youtube.com/user/Chamalowat"><i className="fab fa-youtube"/></a>
+      </span>
     </footer>
   );
 }
